dc736ba46ffffe444be3e47574c8b32e6f112c9b
Wilson <makoaewilson@gmail.com>
2020-05-06 22:33:47 +0200

    added columns  on campaign explore

--- grecerola/campaign/urls.py
+++ grecerola/campaign/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import campaign_home
+from .views import campaign_explore
 
 urlpatterns = [
     path('', campaign_home, name='campaign-home'),
-]
+    path('explore/', campaign_explore, name='explore-campaign'),
+] 
