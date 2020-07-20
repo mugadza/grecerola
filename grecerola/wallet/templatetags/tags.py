9be0884b1c72468beb9bf6a005b158e40f3af6b0
lmbele <lmbele@altair.co.za>
2020-07-20 09:24:47 +0200

    fixed money tag None type error

--- grecerola/wallet/templatetags/tags.py
+++ grecerola/wallet/templatetags/tags.py
@@ -13,7 +13,9 @@ def is_positive(value):
 @register.filter()
 def money_value(value):
     result = 0
-    if value < 0:
+    if value == None:
+        result = "R0.00"
+    elif value < 0:
         result = "-R" + str(-1*value)
     else:
         result = "R" + str(value)
