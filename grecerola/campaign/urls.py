c963cc69184c1d1df8de3c19dca90fda40af4ef5
Mbele Lebohang <mblleb006@myuct.ac.za>
2020-08-17 21:11:49 +0200

    updated campaign list and details

--- grecerola/campaign/urls.py
+++ grecerola/campaign/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     campaign_home,
     campaign_detail,
+    explore,
     campaign_management,
     CampaignListView,
     investment_detail,
@@ -13,11 +14,11 @@ from .views import (
 urlpatterns = [
     path('', campaign_home, name='campaign-home'),
     path('management/', campaign_management, name='campaign-management'),
-    path('explore/', CampaignListView.as_view(), name='explore'),
-    path('explore/<typename>', CampaignListView.as_view()),
+    path('explore/', explore, name='explore'),
+    path('explore/<slug:campaign_type_slug>/', explore, name='explore-by-campaign-type'),
     path('contact/', contact_us, name='contact-us'),
     path('faq/', faq, name='faq'),
     path('about/', about, name='about'),
-    path('explore/campaign-no=?<int:pk>/', campaign_detail, name='campaign-detail'),
+    path('explore/<int:pk>/<slug:slug>/', campaign_detail, name='campaign-detail'),
     path('invest=?<int:pk>/', investment_detail, name='investment-detail')
 ]
--- grecerola/campaign/views.py
+++ grecerola/campaign/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 from .models import Campaign, CampaignType
+from . import CampaignStatus
 
 def campaign_home(request):
     return render(request, "campaign/index.html")
@@ -17,10 +21,26 @@ def faq(request):
 def about(request):
     return  render(request,"campaign/about.html")
 
-def campaign_detail(request, pk):
-    campaign = get_object_or_404 (Campaign, pk=pk)
+def explore(request, campaign_type_slug=None):
+    campaign_type = None
+    campaign_types = CampaignType.objects.all()
+    campaigns = Campaign.objects.filter(Q(is_published=True), Q(status=CampaignStatus.UNFULFILLED)) 
+
+    if campaign_type_slug:
+        campaign_type = get_object_or_404(CampaignType, slug=campaign_type_slug)
+        campaigns = campaigns.filter(campaign_type=campaign_type)
+
+    return render(request, "campaign/explore.html", {
+        'campaign_type': campaign_type,
+        'campaign_types': campaign_types,
+        'campaigns': campaigns
+    })
+
+def campaign_detail(request, pk, slug):
+    campaign = get_object_or_404 (Campaign, pk=pk, slug=slug, is_published=True)
     return render(request,"campaign/campaign_detail.html", {'campaign':campaign, 'images': campaign.images.all()})
 
+@login_required
 def investment_detail(request, pk):
     campaign = get_object_or_404(Campaign, pk=pk)
     return render(request,"campaign/investment_detail.html", {'campaign':campaign})
